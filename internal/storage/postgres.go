f171d289aa1605f0f3dfcbb83b845e7033c36d0b
Popolzen <zab-nikita@yandex.ru>
2026-01-22 20:34:04 +0300

    added all  postgres-repo methods

--- internal/storage/postgres.go
+++ internal/storage/postgres.go
@@ -3,12 +3,14 @@ package storage
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"time"
 
 	"github.com/Popolzen/go_final_project/internal/models"
 	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
+	"github.com/lib/pq"
 	_ "github.com/lib/pq"
 )
 
@@ -45,9 +47,11 @@ func (r *PostgresRepository) CreateUser(ctx context.Context, user *models.User) 
 
 	if err != nil {
 		// Проверка на дубликат username
-		if err.Error() == `pq: duplicate key value violates unique constraint "users_username_key"` {
+		var pqErr *pq.Error
+		if errors.As(err, &pqErr) && pqErr.Code == "23505" {
 			return models.ErrUserExists
 		}
+
 		return fmt.Errorf("failed to create user: %w", err)
 	}
 	return nil
@@ -59,7 +63,7 @@ func (r *PostgresRepository) GetUserByUsername(ctx context.Context, username str
 	query := `SELECT * FROM users WHERE username = $1`
 
 	err := r.db.GetContext(ctx, &user, query, username)
-	if err == sql.ErrNoRows {
+	if errors.Is(err, sql.ErrNoRows) {
 		return nil, models.ErrUserNotFound
 	}
 	if err != nil {
@@ -75,9 +79,10 @@ func (r *PostgresRepository) GetUserByID(ctx context.Context, id uuid.UUID) (*mo
 	query := `SELECT * FROM users WHERE id = $1`
 
 	err := r.db.GetContext(ctx, &user, query, id)
-	if err == sql.ErrNoRows {
+	if errors.Is(err, sql.ErrNoRows) {
 		return nil, models.ErrUserNotFound
 	}
+
 	if err != nil {
 		return nil, fmt.Errorf("failed to get user: %w", err)
 	}
@@ -91,4 +96,123 @@ func (r *PostgresRepository) Ping(ctx context.Context) error {
 
 func (r *PostgresRepository) Close() error {
 	return r.db.Close()
+}
+
+func (r *PostgresRepository) CreateSecret(ctx context.Context, secret *models.Secret) error {
+	query := `
+		INSERT INTO secrets (user_id, type, name, data, metadata)
+		VALUES ($1, $2, $3, $4, $5)
+		RETURNING id, version, created_at, updated_at`
+
+	err := r.db.QueryRowContext(ctx, query,
+		secret.UserID, secret.Type, secret.Name, secret.Data, secret.Metadata).
+		Scan(&secret.ID, &secret.Version, &secret.CreatedAt, &secret.UpdatedAt)
+
+	if err != nil {
+		return fmt.Errorf("failed to create secret: %w", err)
+	}
+	return nil
+}
+
+func (r *PostgresRepository) UpdateSecret(ctx context.Context, secret *models.Secret) error {
+	query := `
+		UPDATE secrets
+		SET data = $1, metadata = $2, version = version + 1
+		WHERE id = $3 AND user_id = $4 AND deleted_at IS NULL
+		RETURNING version, updated_at`
+
+	err := r.db.QueryRowContext(ctx, query,
+		secret.Data, secret.Metadata, secret.ID, secret.UserID).
+		Scan(&secret.Version, &secret.UpdatedAt)
+
+	if errors.Is(err, sql.ErrNoRows) {
+		return models.ErrSecretNotFound
+	}
+
+	if err != nil {
+		return fmt.Errorf("failed to update secret: %w", err)
+	}
+	return nil
+}
+
+func (r *PostgresRepository) DeleteSecret(ctx context.Context, id, userID uuid.UUID) error {
+	query := `
+		UPDATE secrets
+		SET deleted_at = NOW()
+		WHERE id = $1 AND user_id = $2 AND deleted_at IS NULL
+	`
+
+	result, err := r.db.ExecContext(ctx, query, id, userID)
+	if err != nil {
+		return fmt.Errorf("failed to delete secret: %w", err)
+	}
+
+	rows, err := result.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("failed to get rows affected: %w", err)
+	}
+
+	if rows == 0 {
+		return models.ErrSecretNotFound
+	}
+
+	return nil
+}
+
+// GetUserByUsername получает пользователя по username
+func (r *PostgresRepository) GetSecret(ctx context.Context, id, userID uuid.UUID) (*models.Secret, error) {
+	var secret models.Secret
+
+	query := `SELECT * FROM users WHERE id = $1 AND user_id = $2 AND deleted_at IS NULL`
+
+	err := r.db.GetContext(ctx, &secret, query, id, userID)
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, models.ErrUserNotFound
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to get secret: %w", err)
+	}
+
+	return &secret, nil
+}
+
+func (r *PostgresRepository) GetSecretByName(ctx context.Context, name string, secretType models.SecretType, userID uuid.UUID) (*models.Secret, error) {
+	var secret models.Secret
+
+	query := `SELECT * FROM users WHERE name = $1 AND type = $2 AND user_id = $3 AND deleted_at IS NULL`
+
+	err := r.db.GetContext(ctx, &secret, query, name, secretType, userID)
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, models.ErrUserNotFound
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to get secret by name: %w", err)
+	}
+
+	return &secret, nil
+
+}
+
+func (r *PostgresRepository) ListSecrets(ctx context.Context, userID uuid.UUID) ([]*models.Secret, error) {
+	var secrets []*models.Secret
+	query := `SELECT * FROM secrets WHERE user_id = $1 AND deleted_at IS NULL ORDER BY created_at DESC`
+
+	err := r.db.SelectContext(ctx, &secrets, query, userID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list secrets: %w", err)
+	}
+
+	return secrets, nil
+}
+
+func (r *PostgresRepository) GetSecretsAfter(ctx context.Context, userID uuid.UUID, after time.Time) ([]*models.Secret, error) {
+	var secrets []*models.Secret
+	query := `SELECT * FROM secrets WHERE user_id = $1 AND updated_at > $2 ORDER BY updated_at`
+
+	err := r.db.SelectContext(ctx, &secrets, query, userID, after)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get secrets after: %w", err)
+	}
+
+	return secrets, nil
 }
--- internal/storage/repository.go
+++ internal/storage/repository.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"context"
+	"time"
 
 	"github.com/Popolzen/go_final_project/internal/models"
 	"github.com/google/uuid"
@@ -12,13 +13,13 @@ type Repository interface {
 	GetUserByUsername(ctx context.Context, username string) (*models.User, error)
 	GetUserByID(ctx context.Context, id uuid.UUID) (*models.User, error)
 
-	// CreateSecret(ctx context.Context, secret *models.Secret) error
-	// UpdateSecret(ctx context.Context, secret *models.Secret) error
-	// DeleteSecret(ctx context.Context, id, userID uuid.UUID) error
-	// GetSecret(ctx context.Context, id, userID uuid.UUID) (*models.Secret, error)
-	// GetSecretByName(ctx context.Context, name string, secretType models.SecretType, userID uuid.UUID) (*models.Secret, error)
-	// ListSecrets(ctx context.Context, userID uuid.UUID) ([]*models.Secret, error)
-	// GetSecretsAfter(ctx context.Context, userID uuid.UUID, after time.Time) ([]*models.Secret, error)
+	CreateSecret(ctx context.Context, secret *models.Secret) error
+	UpdateSecret(ctx context.Context, secret *models.Secret) error
+	DeleteSecret(ctx context.Context, id, userID uuid.UUID) error
+	GetSecret(ctx context.Context, id, userID uuid.UUID) (*models.Secret, error)
+	GetSecretByName(ctx context.Context, name string, secretType models.SecretType, userID uuid.UUID) (*models.Secret, error)
+	ListSecrets(ctx context.Context, userID uuid.UUID) ([]*models.Secret, error)
+	GetSecretsAfter(ctx context.Context, userID uuid.UUID, after time.Time) ([]*models.Secret, error)
 
 	Ping(ctx context.Context) error
 	Close() error
